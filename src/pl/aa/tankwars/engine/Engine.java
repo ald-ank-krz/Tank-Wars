f1d1a759937c6328ac00274c64f0c133caee729f
ankaka <ankasz007@tlen.pl>
2012-09-13 11:58:20 +0200

    Żeby nie było, że nic nie robię. Co nie zmienia faktu, że niewiele rozumiem :D

---
+++ src/pl/aa/tankwars/engine/Engine.java
@@ -0,0 +1,14 @@
+package pl.aa.tankwars.engine;
+
+import pl.aa.tankwars.shared.GameEngine;
+
+public class Engine implements GameEngine{
+	private int size;
+	//private BoardUpdateListener l;
+	public void setEngineParams(int size){
+		//
+	}
+	//public void setBoardUpdateListener(BoardUpdateListener l){
+	//
+	//}
+}
