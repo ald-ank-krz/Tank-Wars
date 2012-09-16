5ef7a4a8fcd57df1091acc8037ea4dae313b52f8
ankaka <ankasz007@tlen.pl>
2012-09-16 15:59:19 +0200

    próba aktualizacji

--- src/pl/aa/tankwars/engine/GameObject.java
+++ src/pl/aa/tankwars/engine/GameObject.java
@@ -1,6 +1,9 @@
 package pl.aa.tankwars.engine;
 
 public class GameObject {
+	//jest to klasa, która bêdzie przechowywa³a dane dotycz¹ce jakiegoœ obiektu gry,
+	//z tej klasy bêd¹ dziedziczyæ klasy przedstawiaj¹ce poszczególne obiekty, jak czo³g,
+	//pole wolne lub pole zajête lub w przysz³oœci inny obiekt, który mo¿e byæ elementem planszy
 	Position position;
 	public void setPosition(Position position)
 	{
